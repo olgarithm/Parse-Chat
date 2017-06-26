ad4621c1d35d262531fb3293bf38b8e9d8fc4026
Olga Andreeva <olgarithm@fb.com>
2017-06-26 16:03:47 -0700

    Milestone 3: Send a Chat Message

---
+++ Parse Chat/ChatCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatCell.swift
+//  Parse Chat
+//
+//  Created by Olga Andreeva on 6/26/17.
+//  Copyright © 2017 Olga Andreeva. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
